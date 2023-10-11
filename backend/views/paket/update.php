4e79a566caece42f0883901f5cc1f53ffde1584f
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2023-10-11 08:12:29 +0700

    Fix (backend) : perbaikan tampilan update dan detail paket wedding

--- backend/views/paket/update.php
+++ backend/views/paket/update.php
@@ -5,14 +5,13 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var app\models\Paket $model */
 
-$this->title = 'Update Paket: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Pakets', 'url' => ['index']];
+$this->title = 'Update Paket: ' . $model->nama;
+$this->params['breadcrumbs'][] = ['label' => 'List Paket', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="paket-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
--- backend/views/paket/view.php
+++ backend/views/paket/view.php
@@ -6,18 +6,17 @@ use yii\widgets\DetailView;
 /** @var yii\web\View $this */
 /** @var app\models\Paket $model */
 
-$this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Pakets', 'url' => ['index']];
+$this->title = $model->nama;
+$this->params['breadcrumbs'][] = ['label' => 'List Paket', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="paket-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('Edit', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Hapus', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
@@ -29,7 +28,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
             'hotel_id',
             'kategori',
             'nama',
