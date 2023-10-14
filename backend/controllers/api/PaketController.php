8d2cdad5f2d5f73261b05b6ab942794766c85f9b
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2023-10-15 02:42:02 +0700

    Feature (API) : Create API List Paket Wedding

---
+++ backend/controllers/api/PaketController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\controllers\api;
+
+/**
+* This is the class for REST controller "HotelController".
+*/
+
+use app\models\Paket;
+use yii\filters\AccessControl;
+use yii\helpers\ArrayHelper;
+
+class PaketController extends RestController
+{
+
+    public function actionList()
+    {
+        return $this->output(Paket::find()->all());
+    }
+}
