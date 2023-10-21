12be174d93d8ac465b2a338e2b936b57e76c064a
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2023-10-21 17:50:15 +0700

    Fix (frontend) : implementasi integrasi API untuk get list paket

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
-import { FaMoneyBillAlt } from 'react-icons/fa';
+import React, { useEffect, useState } from "react";
+import { FaMoneyBillAlt } from "react-icons/fa";
+import { paketService } from "../services";
+import { Paket } from "../types";
+import { convertRupiahs } from "../components/ConvertRupiah/indexConverRupiah";
 
 const Home: React.FC = () => {
+  const [paket, setPaket] = useState<Array<Paket>>();
+  const getPaket = async () => {
+    try {
+      const response = await paketService.location();
+      setPaket(response?.data?.data);
+      // response?.data.map((paket: any) => console.log(paket?.data));
+      // console.log("response", response.data.data);
+      // console.log(response.data.username)
+      // console.log("responpetugas",(petugas))
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+  useEffect(() => {
+    getPaket();
+  });
   return (
     <>
       <div className="bg-[url('./background-wijaya-wedding.png')] bg-fixed bg-cover bg-no-repeat  h-screen static  ">
@@ -9,11 +28,17 @@ const Home: React.FC = () => {
           <div className="container mx-auto flex flex-col-reverse justify-center  md:flex-row h-screen md:items-center px-4 md:px-0 text-center">
             <div className="w-full md:w-11/12 flex flex-col  text-white items-center ">
               <div className="w-full ">
-                <h1 className=" font-poppins-bold text-xl  ">Wedding Organizer</h1>
+                <h1 className=" font-poppins-bold text-xl  ">
+                  Wedding Organizer
+                </h1>
               </div>
               <div className="w-full mt-2 ">
-                <h1 className="font-poppins-medium  text-7xl ">Made Perfect </h1>
-                <h1 className="font-poppins-medium  text-7xl ">Your Dream Wedding </h1>
+                <h1 className="font-poppins-medium  text-7xl ">
+                  Made Perfect{" "}
+                </h1>
+                <h1 className="font-poppins-medium  text-7xl ">
+                  Your Dream Wedding{" "}
+                </h1>
               </div>
             </div>
           </div>
@@ -21,18 +46,20 @@ const Home: React.FC = () => {
       </div>
       <div className="container mx-auto mt-24">
         <div className="flex gap-10">
-          <img src="/assets/img/about.png"></img>
+          <img src="/assets/img/about.png" alt="about" />
+
           <div className="flex flex-col justify-center">
             <h1 className="text-[56px]">Tentang Kami</h1>
             <h3 className="text-4xl">
               We are a team of passionate and experienced wedding planners
             </h3>
             <h6 className="text-base mt-4">
-              Our team of wedding planners is passionate about creating unforgettable wedding
-              experiences. We understand that every wedding is unique, and we work closely with our
-              clients to ensure that their vision becomes a reality. Our approach is personal,
-              friendly, and professional, and we strive to make the planning process as stress-free
-              as possible.
+              Our team of wedding planners is passionate about creating
+              unforgettable wedding experiences. We understand that every
+              wedding is unique, and we work closely with our clients to ensure
+              that their vision becomes a reality. Our approach is personal,
+              friendly, and professional, and we strive to make the planning
+              process as stress-free as possible.
             </h6>
             <button className="border border-black w-1/4 rounded-full p-2 mt-4 ">
               Pesan Sekarang
@@ -53,8 +80,9 @@ const Home: React.FC = () => {
             </div>
 
             <div className="mt-1 font-poppins-medium text-base text-[#616161]">
-              Lorem ipsum dolor sit amet consectetur. Augue non malesuada placerat faucibus nam
-              purus sem. Urna pulvinar porttitor dignissim congue pellentesque ac hac.
+              Lorem ipsum dolor sit amet consectetur. Augue non malesuada
+              placerat faucibus nam purus sem. Urna pulvinar porttitor dignissim
+              congue pellentesque ac hac.
             </div>
           </div>
           <div className="rounded-xl bg-white p-10  flex flex-col gap-2 items-start shadow-xl border border-[#f5f5f5]">
@@ -67,8 +95,9 @@ const Home: React.FC = () => {
             </div>
 
             <div className="mt-1 font-poppins-medium text-base text-[#616161]">
-              Lorem ipsum dolor sit amet consectetur. Augue non malesuada placerat faucibus nam
-              purus sem. Urna pulvinar porttitor dignissim congue pellentesque ac hac.
+              Lorem ipsum dolor sit amet consectetur. Augue non malesuada
+              placerat faucibus nam purus sem. Urna pulvinar porttitor dignissim
+              congue pellentesque ac hac.
             </div>
           </div>
           <div className="rounded-xl bg-white p-10  flex flex-col gap-2 items-start shadow-xl border border-[#f5f5f5]">
@@ -81,8 +110,9 @@ const Home: React.FC = () => {
             </div>
 
             <div className="mt-1 font-poppins-medium text-base text-[#616161]">
-              Lorem ipsum dolor sit amet consectetur. Augue non malesuada placerat faucibus nam
-              purus sem. Urna pulvinar porttitor dignissim congue pellentesque ac hac.
+              Lorem ipsum dolor sit amet consectetur. Augue non malesuada
+              placerat faucibus nam purus sem. Urna pulvinar porttitor dignissim
+              congue pellentesque ac hac.
             </div>
           </div>
           <div className="rounded-xl bg-white p-10  flex flex-col gap-2 items-start shadow-xl border border-[#f5f5f5]">
@@ -95,8 +125,9 @@ const Home: React.FC = () => {
             </div>
 
             <div className="mt-1 font-poppins-medium text-base text-[#616161]">
-              Lorem ipsum dolor sit amet consectetur. Augue non malesuada placerat faucibus nam
-              purus sem. Urna pulvinar porttitor dignissim congue pellentesque ac hac.
+              Lorem ipsum dolor sit amet consectetur. Augue non malesuada
+              placerat faucibus nam purus sem. Urna pulvinar porttitor dignissim
+              congue pellentesque ac hac.
             </div>
           </div>
         </div>
@@ -162,118 +193,39 @@ const Home: React.FC = () => {
       <div className="container mx-auto mt-24">
         <h1 className="text-center text-4xl">Paket Wedding</h1>
         <div className=" py-10 grid grid-cols-1 gap-10 md:grid-cols-4  sm:w-auto ">
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img
-              className="w-full"
-              src="/assets/img/dummy-paket-wedding.png"
-              alt="Sunset in the mountains"
-            />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-                Maiores et perferendis eaque, exercitationem praesentium nihil.
-              </p>
+          {paket?.map((list, index) => (
+            <div className="max-w-sm rounded overflow-hidden shadow-lg ">
+              <img
+                className="w-full h-2/4 object-cover"
+                src={
+                  list.gambar
+                    ? `http://localhost/fullstack-wijaya-wedding-github-amin/backend/uploads/image/paket/${list.gambar}`
+                    : `/assets/img/dummy-paket-wedding.png`
+                }
+                alt={list.gambar}
+              />
+              <div className="px-6 pt-6 h-full">
+                <div className="font-bold text-2xl mb-1 capitalize">
+                  {list.kategori === "hotel"
+                    ? list.hotel_detail.nama
+                    : list.kategori}
+                </div>
+                <p className="font-bold text-base mb-2 capitalize">
+                  {list.nama}
+                </p>
+
+                <p className=" text-base capitalize">
+                  {convertRupiahs.format(list.harga)}
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img
-              className="w-full"
-              src="/assets/img/dummy-paket-wedding.png"
-              alt="Sunset in the mountains"
-            />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-                Maiores et perferendis eaque, exercitationem praesentium nihil.
-              </p>
-            </div>
-          </div>
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img
-              className="w-full"
-              src="/assets/img/dummy-paket-wedding.png"
-              alt="Sunset in the mountains"
-            />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-                Maiores et perferendis eaque, exercitationem praesentium nihil.
-              </p>
-            </div>
-          </div>
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img
-              className="w-full"
-              src="/assets/img/dummy-paket-wedding.png"
-              alt="Sunset in the mountains"
-            />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-                Maiores et perferendis eaque, exercitationem praesentium nihil.
-              </p>
-            </div>
-          </div>
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img
-              className="w-full"
-              src="/assets/img/dummy-paket-wedding.png"
-              alt="Sunset in the mountains"
-            />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-                Maiores et perferendis eaque, exercitationem praesentium nihil.
-              </p>
-            </div>
-          </div>
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img
-              className="w-full"
-              src="/assets/img/dummy-paket-wedding.png"
-              alt="Sunset in the mountains"
-            />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-                Maiores et perferendis eaque, exercitationem praesentium nihil.
-              </p>
-            </div>
-          </div>
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img
-              className="w-full"
-              src="/assets/img/dummy-paket-wedding.png"
-              alt="Sunset in the mountains"
-            />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-                Maiores et perferendis eaque, exercitationem praesentium nihil.
-              </p>
-            </div>
-          </div>
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img
-              className="w-full"
-              src="/assets/img/dummy-paket-wedding.png"
-              alt="Sunset in the mountains"
-            />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-                Maiores et perferendis eaque, exercitationem praesentium nihil.
-              </p>
-            </div>
-          </div>
+          ))}
+        </div>
+        <div className="w-full  flex justify-center mt-4">
+          <button className="btn bg-yellow-600 py-3 px-5 text-white font-bold rounded-full">
+            {" "}
+            Paket Lainnya{" "}
+          </button>
         </div>
       </div>
       <div className="container mx-auto mt-24">
