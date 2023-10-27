0fb23c0f6560b10103b056efa819ca44a906c692
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2023-10-28 00:37:36 +0700

    Feature (frontend) : slicing histroy transaksi page

---
+++ frontend/src/pages/Transaksi/History.tsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import { FaMoneyBillAlt } from "react-icons/fa";
+import { Table } from "flowbite-react";
+
+const History: React.FC = () => {
+  //   const [paket, setPaket] = useState<Array<Paket>>([]);
+  //   const getPaket = async () => {
+  //     try {
+  //       const response = await paketService.location();
+  //       // console.log("response", response?.data);
+  //       setPaket(response?.data);
+  //       // response?.data.map((paket: any) => console.log(paket?.data));
+  //       // console.log("response", response.data.data);
+  //       // console.log(response.data.username)
+  //       // console.log("responpetugas",(petugas))
+  //     } catch (error) {
+  //       console.log("error", error);
+  //     }
+  //   };
+  useEffect(() => {
+    // getPaket();
+    // console.log({ paket });
+  });
+  return (
+    <>
+      <div className="container mx-auto mt-32 px-6 md:px-0">
+        <h1 className="text-center text-4xl">History Transaksi</h1>
+        <div className="mt-6 ">
+          <Table hoverable>
+            <Table.Head>
+              <Table.HeadCell>Product name</Table.HeadCell>
+              <Table.HeadCell>Color</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Price</Table.HeadCell>
+              <Table.HeadCell>
+                <span className="sr-only">Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  Apple MacBook Pro 17"
+                </Table.Cell>
+                <Table.Cell>Sliver</Table.Cell>
+                <Table.Cell>Laptop</Table.Cell>
+                <Table.Cell>$2999</Table.Cell>
+                <Table.Cell>
+                  <a
+                    className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                    href="/tables"
+                  >
+                    <p>Edit</p>
+                  </a>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  <p>Microsoft Surface Pro</p>
+                </Table.Cell>
+                <Table.Cell>White</Table.Cell>
+                <Table.Cell>Laptop PC</Table.Cell>
+                <Table.Cell>$1999</Table.Cell>
+                <Table.Cell>
+                  <a
+                    className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                    href="/tables"
+                  >
+                    <p>Edit</p>
+                  </a>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  Magic Mouse 2
+                </Table.Cell>
+                <Table.Cell>Black</Table.Cell>
+                <Table.Cell>Accessories</Table.Cell>
+                <Table.Cell>$99</Table.Cell>
+                <Table.Cell>
+                  <a
+                    className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                    href="/tables"
+                  >
+                    <p>Edit</p>
+                  </a>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default History;
