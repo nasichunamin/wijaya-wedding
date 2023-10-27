b83d97036fd96f0fab470c32bebea0c565319483
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2023-10-28 00:31:25 +0700

    Fix (API) : Paket

--- backend/controllers/api/PaketController.php
+++ backend/controllers/api/PaketController.php
@@ -40,6 +40,32 @@ class PaketController extends RestController
            ],
        ];
    }
-        return $this->output(['message' => 'berhasil', 'data' => $data]);
+        return $this->output($data);
+    }
+
+    public function actionDetail($id){
+        $paket = Paket::findOne(['id'=> $id]);
+     
+            $data = [
+                'id' => $paket->id,
+                'hotel_id' => $paket->hotel_id,
+                'kategori' => $paket->kategori,
+                'nama' => $paket->nama,
+                'harga' => $paket->harga,
+                'gambar' => $paket->gambar,
+                'dekorasi' => $paket->dekorasi,
+                'makeup_dan_busana' => $paket->makeup_dan_busana,
+                'upacara_dan_tenda' => $paket->upacara_dan_tenda,
+                'dokumentasi' => $paket->dokumentasi,
+                'entertainment' => $paket->entertainment,
+                'updateAt' => $paket->updateAt,
+                'deleteAt' => $paket->deleteAt,
+                'hotel_detail' => [
+                    'id' => $paket->hotel->id,
+                    'nama' => $paket->hotel->nama,
+                    'alamat' => $paket->hotel->alamat,
+                ],
+            ];
+             return $this->output($data);
     }
 }
