2e8f8ba5366cf29ffb11189d2473be7ee9e093a7
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2023-10-28 00:35:33 +0700

    Fix (frontend) : perbaikan url link navbar

--- frontend/src/components/Layout/Navbar.tsx
+++ frontend/src/components/Layout/Navbar.tsx
@@ -79,7 +79,7 @@ export function Navbar() {
             </li>
             <li className={`hover:duration-200 py-3 pr-1 pl-1 `}>
               <a
-                href="/about"
+                href="/Paket"
                 className={`block rounded   font-poppins-medium text-base hover:no-underline  ${
                   showMobileNav
                     ? " md:text-[#e8bd22] text-black hover:text-[#e8bd22] "
@@ -154,7 +154,7 @@ export function Navbar() {
 
             <li>
               <a
-                href="/contact"
+                href="/login"
                 className="mt-2 block rounded-full border border-black py-1 text-center font-poppins-bold text-base text-black md:mt-1 md:py-2.5 md:px-5 hover:text-black hover:no-underline hover:bg-[#e8bd22] hover:border-[#e8bd22]"
               >
                 Masuk
