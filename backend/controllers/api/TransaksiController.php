7b045e49a7f014dd923f5f853f7b3c2295d4f4c3
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2023-11-01 00:04:54 +0700

    Fix (API) : Perbaikan response list transaksi by id customer

--- backend/controllers/api/TransaksiController.php
+++ backend/controllers/api/TransaksiController.php
@@ -15,13 +15,52 @@ use yii\web\UploadedFile;
 class TransaksiController extends RestController
 {
 
-    public function actionList()
+    public function actionList($id)
     {
         $currentUser = $this->getAuthUser();
         if (isset($currentUser['code']) && $currentUser['code'] == 500) {
             return $currentUser;
         }
-        return $this->output(Transaksi::find()->all());
+        $lists = Transaksi::find()->where(['account_id' => $id])->all();
+        $data = [];
+
+        foreach ($lists as $list){
+            $data[] = [
+                'id' => $list->id,
+                'account_id' => $list->account_id,
+                'paket_id' => $list->paket_id,
+                'tgl_pemesanan' => $list->tgl_pemesanan,
+                'status' => $list->status,
+                'alamat_pemesanan' => $list->alamat_pemesanan,
+                'bukti_pembayaran' => $list->bukti_pembayaran,
+                'tgl_pembayaran' => $list->tgl_pembayaran,
+                'paket_detail' => [
+                    'id' => $list->paket->id,
+                    'hotel_id' => $list->paket->hotel_id,
+                    'kategori' => $list->paket->kategori,
+                    'nama' => $list->paket->nama,
+                    'harga' => $list->paket->harga,
+                    'gambar' => $list->paket->gambar,
+                    'dekorasi' => $list->paket->dekorasi,
+                    'makeup_dan_busana' => $list->paket->makeup_dan_busana,
+                    'upacara_dan_tenda' => $list->paket->upacara_dan_tenda,
+                    'dokumentasi' => $list->paket->dokumentasi,
+                    'entertainment' => $list->paket->entertainment,
+                    'updateAt' => $list->paket->updateAt,
+                    'deleteAt' => $list->paket->deleteAt,
+                    'hotel_detail' => [
+                        'id' => $list->paket->hotel->id,
+                        'nama' => $list->paket->hotel->nama,
+                        'alamat' => $list->paket->hotel->alamat,
+                    ],
+                ]
+            ];
+        }
+        return $this->output(['message' => 'sukses','data' => $data]);
+
+            
+        
+        // return $this->output(Transaksi::find()->all());
     }
 
     public function actionCreate()
