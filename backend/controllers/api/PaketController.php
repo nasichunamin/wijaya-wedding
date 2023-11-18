5030be1c3e267a634bb852ee01750c551152d7e1
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2023-11-19 00:56:20 +0700

    Fix paket hotel id null

--- backend/controllers/api/PaketController.php
+++ backend/controllers/api/PaketController.php
@@ -12,33 +12,50 @@ use yii\helpers\ArrayHelper;
 
 class PaketController extends RestController
 {
-
     public function actionList()
     {
    $pakets = Paket::find()->all();
    $data = [];
 
    foreach ($pakets as $paket) {
-       $data[] = [
-           'id' => $paket->id,
-           'hotel_id' => $paket->hotel_id,
-           'kategori' => $paket->kategori,
-           'nama' => $paket->nama,
-           'harga' => $paket->harga,
-           'gambar' => $paket->gambar,
-           'dekorasi' => $paket->dekorasi,
-           'makeup_dan_busana' => $paket->makeup_dan_busana,
-           'upacara_dan_tenda' => $paket->upacara_dan_tenda,
-           'dokumentasi' => $paket->dokumentasi,
-           'entertainment' => $paket->entertainment,
-           'updateAt' => $paket->updateAt,
-           'deleteAt' => $paket->deleteAt,
-           'hotel_detail' => [
-               'id' => $paket->hotel->id,
-               'nama' => $paket->hotel->nama,
-               'alamat' => $paket->hotel->alamat,
-           ],
-       ];
+      if($paket->hotel_id !== null) {
+        $data[] = [
+            'id' => $paket->id,
+            'hotel_id' => $paket->hotel_id,
+            'kategori' => $paket->kategori,
+            'nama' => $paket->nama,
+            'harga' => $paket->harga,
+            'gambar' => $paket->gambar,
+            'dekorasi' => $paket->dekorasi,
+            'makeup_dan_busana' => $paket->makeup_dan_busana,
+            'upacara_dan_tenda' => $paket->upacara_dan_tenda,
+            'dokumentasi' => $paket->dokumentasi,
+            'entertainment' => $paket->entertainment,
+            'updateAt' => $paket->updateAt,
+            'deleteAt' => $paket->deleteAt,
+            'hotel_detail' => [
+                'id' => $paket->hotel->id,
+                'nama' => $paket->hotel->nama,
+                'alamat' => $paket->hotel->alamat,
+            ],
+        ];
+      }else{
+        $data[] = [
+            'id' => $paket->id,
+            'hotel_id' => null,
+            'kategori' => $paket->kategori,
+            'nama' => $paket->nama,
+            'harga' => $paket->harga,
+            'gambar' => $paket->gambar,
+            'dekorasi' => $paket->dekorasi,
+            'makeup_dan_busana' => $paket->makeup_dan_busana,
+            'upacara_dan_tenda' => $paket->upacara_dan_tenda,
+            'dokumentasi' => $paket->dokumentasi,
+            'entertainment' => $paket->entertainment,
+            'updateAt' => $paket->updateAt,
+            'deleteAt' => $paket->deleteAt,
+        ];
+      }
    }
         return $this->output($data);
     }
