d29446bbb114efb1e05f65a94673ac3d4b670ca9
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2023-12-02 22:49:14 +0700

    Fix (frontend) : ganti gambar portofolio di landing page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -238,49 +238,56 @@ const Home: React.FC = () => {
         <div className="container mx-auto mt-24">
           <h1 className="text-center text-4xl">Portofolio</h1>
 
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-10">
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-10 px-4 md:px-0">
             <div className="grid gap-4">
               <div>
                 <img
                   className="h-auto max-w-full rounded-lg"
-                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
+                  src="/assets/img/WJY-280923_-12-min.jpg"
                   alt=""
                 />
               </div>
               <div>
                 <img
                   className="h-auto max-w-full rounded-lg"
-                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"
+                  src="/assets/img/WJY_130823-16-min.jpg"
                   alt=""
                 />
               </div>
               <div>
                 <img
                   className="h-auto max-w-full rounded-lg"
-                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg"
+                  src="/assets/img/WJY-280923_-18-min.jpg"
                   alt=""
                 />
               </div>
             </div>
-            <div className="grid gap-4">
+            <div className="grid gap-6 lg:gap-8 xl:gap-10 h-full">
               <div>
                 <img
                   className="h-auto max-w-full rounded-lg"
-                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg"
+                  src="/assets/img/WJY_130823-12-min.jpg"
                   alt=""
                 />
               </div>
               <div>
                 <img
                   className="h-auto max-w-full rounded-lg"
-                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg"
+                  src="/assets/img/WJY-280923_-16-min.jpg"
                   alt=""
                 />
               </div>
               <div>
                 <img
                   className="h-auto max-w-full rounded-lg"
-                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"
+                  src="/assets/img/WJY_130823-20-min.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  className="h-auto max-w-full rounded-lg"
+                  src="/assets/img/WJY-280923_-47-min.jpg"
                   alt=""
                 />
               </div>
@@ -289,21 +296,21 @@ const Home: React.FC = () => {
               <div>
                 <img
                   className="h-auto max-w-full rounded-lg"
-                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg"
+                  src="/assets/img/WJY_130823-31-min.jpg"
                   alt=""
                 />
               </div>
               <div>
                 <img
                   className="h-auto max-w-full rounded-lg"
-                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg"
+                  src="/assets/img/WJY_130823-18-min.jpg"
                   alt=""
                 />
               </div>
               <div>
                 <img
                   className="h-auto max-w-full rounded-lg"
-                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"
+                  src="/assets/img/WJYy-06328-min.jpg"
                   alt=""
                 />
               </div>
